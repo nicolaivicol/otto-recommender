d828935c02de86b5b375fd9b10c96f2c83e6110e
nicolaiv <nicolaiv@wix.com>
2023-01-16 19:47:23 +0200

    allow for larger window for co-events

--- config.py
+++ config.py
@@ -35,43 +35,46 @@ ID2TYPE = {0: 'clicks', 1: 'carts', 2: 'orders'}
 # df['type'].map(type2id) # df['type'].map(lambda i: id2type[i])
 
 # filter co-events when doing self merge
-MIN_TIME_TO_NEXT = 0  # value zero means that next event can't be before this event
-MAX_TIME_TO_NEXT = 1 * 24 * 60 * 60  # 1 day * 24 hours * 60 min * 60 sec
-MAX_TIME_TO_NEXT_CLICK_TO_CLICK = 3 * 60 * 60  # 3 hours * 60 min * 60 sec
+MIN_TIME_TO_NEXT = -24 * 60 * 60  # value zero means that next event can't be before this event
+MAX_TIME_TO_NEXT = 24 * 60 * 60  # 24 hours * 60 min * 60 sec
+MAX_TIME_TO_NEXT_CLICK_TO_CLICK = 12 * 60 * 60  # 12 hours * 60 min * 60 sec
 
 OPTIM_ROWS_POLARS_GROUPBY = 100_000_000
-MAX_ROWS_POLARS_GROUPBY = 350_000_000
+MAX_ROWS_POLARS_GROUPBY = 300_000_000
 
 # minimum count to be saved on disk
 MIN_COUNT_TO_SAVE = {
     'count_click_to_click': 10,
     'count_click_to_cart_or_buy': 5,
-    'count_cart_to_buy': 2,
     'count_cart_to_cart': 2,
+    'count_cart_to_buy': 2,
     'count_buy_to_buy': 2,
 }
-MAX_CO_EVENT_PAIRS_TO_SAVE_DISK = 20_000_000
+MIN_COUNT_IN_PART = {'count_click_to_click': 2, 'count_click_to_cart_or_buy': 2}
+MAX_CO_EVENT_PAIRS_TO_SAVE_DISK = 300_000_000
 
 # which counts to compute
 CO_EVENTS_TO_COUNT = [
     'count_click_to_click',
     'count_click_to_cart_or_buy',
-    'count_cart_to_buy',
     'count_cart_to_cart',
+    'count_cart_to_buy',
     'count_buy_to_buy',
 ]
 
 # RETRIEVAL
 # to retrieve candidates co-events, keep only the last N events in session
-RETRIEVE_N_LAST_CLICKS = 30  # percentile 99%
-RETRIEVE_N_LAST_CARTS = 25  # percentile 99.5%
-RETRIEVE_N_LAST_ORDERS = 25  # percentile 99.5%
+# use a high number to keep all
+RETRIEVE_N_LAST_CLICKS = 30  # 30: percentile 99%
+RETRIEVE_N_LAST_CARTS = 999  # 25: percentile 99.5%
+RETRIEVE_N_LAST_ORDERS = 999  # 25: percentile 99.5%
 
 MAP_NAME_COUNT_TYPE = {
     # (event type to next event type(s))
     'click_to_click': (0, [0]),
     'click_to_cart_or_buy': (0, [1, 2]),
     'cart_to_cart': (1, [1]),
+    'cart_to_buy': (1, [2]),
     'buy_to_buy': (2, [2]),
 }
 
@@ -79,6 +82,7 @@ RETRIEVAL_FIRST_N_CO_COUNTS = {
     'click_to_click': 10,
     'click_to_cart_or_buy': 10,
     'cart_to_cart': 20,
+    'cart_to_buy': 20,
     'buy_to_buy': 20,
 }
 
@@ -86,6 +90,7 @@ RETRIEVAL_CO_COUNTS_TO_JOIN = [
     'click_to_click',
     'click_to_cart_or_buy',
     'cart_to_cart',
+    # 'cart_to_buy',
     'buy_to_buy',
 ]
 
