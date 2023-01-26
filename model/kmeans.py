502d759e13af5eab20b06472254255a7fda32613
nicolaiv <nicolaiv@wix.com>
2023-01-26 11:13:51 +0200

    refactor kmeans to use dask array and hdf5 files

--- model/kmeans.py
+++ model/kmeans.py
@@ -1,3 +1,4 @@
+import numpy
 import pandas as pd
 from tqdm import tqdm
 import json
@@ -6,6 +7,7 @@ import glob
 import logging
 import argparse
 
+import h5py
 import polars as pl
 import numpy as np
 from sklearn.cluster import KMeans
@@ -24,6 +26,7 @@ set_display_options()
 
 # References:
 # https://realpython.com/k-means-clustering-python/
+# https://dask.pydata.org/en/latest/array-creation.html
 
 
 def load_aid_embeddings(model_name: str) -> pl.DataFrame:
@@ -60,73 +63,98 @@ def compute_sessions_embeddings(
             .with_column(pl.col('ts').max().over('session').alias('max_ts')) \
             .with_column((1 - (pl.col('max_ts') - pl.col('ts')) / (60 * 60 * 24 * 3)).clip_min(0.10).alias('weight_time')) \
             .join(df_weights_by_type, on='type', how='left') \
-            .with_column((pl.col('weight_time') * pl.col('weight_type')).alias('weight')) \
+            .with_column((pl.col('weight_time') * pl.col('weight_type')).cast(pl.Float32).alias('weight')) \
             .drop(['ts', 'type', 'max_ts', 'weight_time', 'weight_type'])
         df_sessions = df_sessions.join(df_embeddings, on='aid', how='left').fill_null(0)  # some aid do not have an embedding
         df_sessions = df_sessions \
             .groupby('session') \
             .agg([pl.sum('weight')] + [(pl.col(col) * pl.col('weight')).sum() for col in cols_embedding]) \
-            .with_columns([pl.col(col) / pl.col('weight') for col in cols_embedding]) \
+            .with_columns([(pl.col(col) / pl.col('weight')).round(6).cast(pl.Float32) for col in cols_embedding]) \
             .select(['session'] + cols_embedding)
         df_sessions = df_sessions \
             .with_column(pl.concat_list(cols_embedding).alias('embedding')) \
-            .select(['session', 'embedding'])
+            .select(['session', 'embedding'])\
+            .sort('session')
 
-        file_name_out = file_sessions.replace('-parquet/', '-sessions-w2vec/')
-        os.makedirs(os.path.dirname(file_name_out), exist_ok=True)
-        df_sessions.write_parquet(file_name_out)
+        # save to .parquet file
+        file_name_out_parquet = file_sessions.replace('-parquet/', '-sessions-w2vec-parquet/')
+        os.makedirs(os.path.dirname(file_name_out_parquet), exist_ok=True)
+        df_sessions.write_parquet(file_name_out_parquet)
+
+        # save to hdf5 file
+        np_embedding = np.array(df_sessions['embedding'].to_list())
+        np_session = df_sessions['session'].to_numpy()
+        file_name_out_hdf5 = file_sessions.replace('-parquet/', '-sessions-w2vec-h5/').replace('.parquet', '.h5')
+        os.makedirs(os.path.dirname(file_name_out_hdf5), exist_ok=True)
+        with h5py.File(file_name_out_hdf5, 'w') as hf:
+            hf.create_dataset('session', data=np_session, dtype='int32')
+            hf.create_dataset('embedding', data=np_embedding, dtype='float32')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_split_alias', default='train-test')
     parser.add_argument('--model_name', default='word2vec-train-test-types-all-size-100-mincount-5-window-10')
-    parser.add_argument('--use_dask', default=False)
+    parser.add_argument('--use_dask', default=True)
     args = parser.parse_args()
+
+    use_dask = args.use_dask
 
     log.info(f'Running {os.path.basename(__file__)} with parameters: \n' + json.dumps(vars(args), indent=2))
     log.info('This finds sessions clusters.')
 
-    dask_client = set_up_dask_client() if args.use_dask else None
+    dask_client = set_up_dask_client() if use_dask else None
 
-    dir_sessions_embeddings = f'{config.DIR_DATA}/{args.data_split_alias}-sessions-w2vec'
+    dir_sessions_embeddings_h5 = f'{config.DIR_DATA}/{args.data_split_alias}-sessions-w2vec-h5'
+    dir_sessions_embeddings_parquet = f'{config.DIR_DATA}/{args.data_split_alias}-sessions-w2vec-parquet'
     dir_out = f'{config.DIR_DATA}/{args.data_split_alias}-sessions-clusters'
     os.makedirs(dir_out, exist_ok=True)
 
-    if not os.path.exists(dir_sessions_embeddings):
+    files_w_embeddings_missing = (
+            (use_dask and not os.path.exists(dir_sessions_embeddings_h5))
+            or (not use_dask and not os.path.exists(dir_sessions_embeddings_parquet)))
+
+    if files_w_embeddings_missing:
         log.info(f'Compute sessions embeddings based on word2vec embeddings of AIDs in the session')
         compute_sessions_embeddings(args.data_split_alias, args.model_name)
 
-    # cols_embedding = [f'dim_{i}' for i in range(100)]
-    # cols_embedding = [col for col in df_embeddings.columns if col.startswith('dim_')]
-
     log.info('Load sessions embeddings')
-    df_embeddings = pl.read_parquet(f'{dir_sessions_embeddings}/*/*.parquet')
-    log.debug(f'Loaded {df_embeddings.shape[0]} rows and {df_embeddings.shape[1]} columns')
 
-    log.info('Prepare data for KMeans')
-    if args.use_dask:
-        # df_embeddings['embedding'].to_numpy()
-        X = dask.array.from_array(np.array(df_embeddings['embedding'].to_list()), chunks='80mb')
+    if use_dask:
+        files_h5 = sorted(glob.glob(f'{dir_sessions_embeddings_h5}/*/*.h5'))
+        vecs_parts = []
+        sessions_parts = []
+
+        for f in files_h5:
+            hf = h5py.File(f)
+            vecs_parts.append(dask.array.from_array(hf['embedding'], chunks=(100_000, 100)))
+            sessions_parts.append(dask.array.from_array(hf['session']))
+
+        X = dask.array.concatenate(vecs_parts)
+        sessions = dask.array.concatenate(sessions_parts).compute()
+        log.debug(f'Scanned {X.shape[0]} rows and {X.shape[1]} columns from {len(files_h5)} .h5 files')
     else:
-        X = df_embeddings['embedding'].to_list()
-
-    # akeyless connect -t data-scientists@ -v ssh.prod-access.wewix.net -c /prod/BI/data-scientists-cert-issuer --tunnel=-L :2222:YOUR_MACHINE
+        df_embeddings = pl.read_parquet(f'{dir_sessions_embeddings_parquet}/*/*.parquet')
+        X = np.array(df_embeddings['embedding'].to_list())
+        sessions = df_embeddings['session']
+        log.debug(f'Loaded {X.shape[0]} rows and {X.shape[1]} columns from .parquet files')
 
     log.info('Fit KMeans')
 
-    lst_n_clusters = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000]
+    lst_n_clusters = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
     res = []
 
     for n_clusters in tqdm(lst_n_clusters, unit='model'):
-        # n_clusters = 1000
-        if args.use_dask:
+        # n_clusters = 100
+        if use_dask:
+            log.debug(f'Init Dask KMeans with: n_clusters={n_clusters}')
             km = dask_ml.cluster.KMeans(
                 n_clusters=n_clusters,
                 max_iter=300,
-                random_state=42
+                random_state=42,
             )
         else:
+            log.debug(f'Init scikit KMeans with: n_clusters={n_clusters}')
             km = KMeans(
                 init='random',
                 n_clusters=n_clusters,
@@ -134,28 +162,22 @@ if __name__ == '__main__':
                 max_iter=300,
                 random_state=42,
             )
-
         km.fit(X)
-        log.info(f'KMeans: n_clusters={n_clusters}, inertia={km.inertia_:.2f}, n_iter={km.n_iter_}')
-        res.append({'n_clusters': n_clusters, 'inertia': km.inertia_, 'n_iter': km.n_iter_})
 
-        # save on disk
-        df_clusters = pl.DataFrame({'session': df_embeddings['session'], 'cluster': km.labels_})
+        log.info(f'KMeans: n_clusters={km.n_clusters}, inertia={km.inertia_:.2f}, n_iter={km.n_iter_}')
+        res.append({'n_clusters': km.n_clusters, 'inertia': km.inertia_, 'n_iter': km.n_iter_})
+        pd.DataFrame(res).to_csv(f'{dir_out}/logs.csv', index=False)
+
+        # save clusters to disk
+        df_clusters = pl.DataFrame({'session': sessions, 'cluster': np.array(km.labels_)},
+                                   columns={'session': pl.Int32, 'cluster': pl.Int16})
         file_out = f'{dir_out}/sessions-clusters-{n_clusters}.parquet'
         df_clusters.write_parquet(file_out)
 
-    pd.DataFrame(res).to_csv(f'{dir_out}/logs.csv', index=False)
+        if use_dask:
+            dask_client.restart()
 
     dask_client.close(60)
-
-    # kmeans.inertia_  # The lowest SSE value
-    # kmeans.n_iter_  # The number of iterations required to converge
-    # kmeans.labels_
-    # # array([1, 1, 1, 0, 0, 0], dtype=int32)
-    # kmeans.predict([[0, 0], [12, 3]])
-    # # array([1, 0], dtype=int32)
-    # kmeans.cluster_centers_
-    # # array([[10.,  2.], [ 1.,  2.]])
 
 # describe_numeric(df_sessions[['n']].to_pandas())
 # df_sessions.filter(pl.col('session') == 4).to_pandas()
